074c5e1022639704368aeaca14fdd9f0d928d8aa
Your Name <you@example.com>
2022-04-07 11:04:58 +0800

    rm toc extension

--- packages/build.sh
+++ packages/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export NODE_OPTIONS=--openssl-legacy-provider
-for i in `ls`; do (cd $i; npm run build; cd ..); done
+for i in `ls`; do [ -d $i ] && (echo $i; cd $i; npm run build; cd ..); done
 
